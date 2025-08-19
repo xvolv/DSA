41b7712c85495fdf705d2e0655c3639c6c5eef81
xvolv <nothingism99@gmail.com>
2025-08-19 23:27:25 +0300

    Time: 345 ms (65.63%) | Memory: 12.6 MB (22.75%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,29 +1,29 @@
-class Solution:
+class Solution(object):
     def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
         if not s:
             return ""
-        
-        start, end = 0, 0
-        
-        def expandAroundCenter(left, right):
+
+        start = end = 0
+
+        def expand(left, right):
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-            return right - left - 1
-        
+            return left + 1, right - 1
+
         for i in range(len(s)):
-            len1 = expandAroundCenter(i, i)
-            len2 = expandAroundCenter(i, i + 1)
-            max_len = max(len1, len2)
-            
-            if max_len > end - start:
-                start = i - (max_len - 1) // 2
-                end = i + max_len // 2
-        
+            # Odd length palindrome
+            l1, r1 = expand(i, i)
+            # Even length palindrome
+            l2, r2 = expand(i, i + 1)
+
+            if r1 - l1 > end - start:
+                start, end = l1, r1
+            if r2 - l2 > end - start:
+                start, end = l2, r2
+
         return s[start:end+1]
-
-
-# Test
-sol = Solution()
-print(sol.longestPalindrome("babad"))  # bab or aba
-print(sol.longestPalindrome("cbbd"))   # bb
